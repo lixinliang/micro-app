d43595b606ea5d3e27b40bf3da476a3830b4a8da
lixinliang <lixinliang@yy.com>
2016-08-22 15:30:45 +0800

    update

--- webpack/concat.js
+++ webpack/concat.js
@@ -4,6 +4,7 @@ let fs = require('fs');
 let path = require('path');
 let fse = require('fs-extra');
 
+// concat util
 let src = path.join(__dirname, '../src/modules/util/');
 let dest = path.join(__dirname, '../src/modules/util.js');
 
@@ -13,6 +14,10 @@ module.exports = new Promise(( resolve, reject ) => {
         if (err) {
             reject(err);
             return
+        }
+        if (process.platform == 'win32') {
+            // put `_variable` first
+            files.unshift(files.pop());
         }
         files.forEach(( file ) => {
             result.push(`//---------- ./${ file } ----------\n`);
